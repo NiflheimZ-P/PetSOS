143a7b62a4140bd9e788db839743e1382de05466
NiflheimZ-P <66070193@kmitl.ac.th>
2025-10-10 11:09:11 +0700

    feat: Api for register

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import bcrypt from 'bcrypt'
+export const POST = async (req: any) => {
+  try {
+
+    const {first_name, last_name, username, email, password} = await req.json();
+
+    if (await prisma.users.findUnique({where:{email:email}})){
+        return NextResponse.json({status:400, message:"Email Already Exist!"});
+    }
+    if (await prisma.users.findUnique({where:{username:username}})){
+        return NextResponse.json({status:400, message:"Username Already Exist!"});
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const createUser = await prisma.users.create({
+      data: {
+        first_name: first_name,
+        last_name: last_name,
+        username: username,
+        email: email,
+        password: hashPassword,
+        role: "USER",
+      },
+    });
+    return NextResponse.json({status:201, message:"User Created Successfully", user:createUser})
+  } catch (error) {
+    return NextResponse.json({status:400, error:error})
+  };
+};
